115477e88aa7235acfc0141e104419b2e1c6fa35
yibre <cdoyue@gmail.com>
2021-05-22 03:30:07 +0900

    doyeon add google map in the post detail

--- visualtravel/posts/urls.py
+++ visualtravel/posts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("<int:pk>/", views.PostDetail.as_view(), name="detail"),
     path("<int:pk>/edit/", views.EditPostView.as_view(), name="edit"),
     path("upload/", views.UploadPostView.as_view(), name="upload"),
+    path("<int:pk>/delete/", views.delete_post, name="delete"),
     path("<int:pk>/photos/", views.PostPhotosView.as_view(), name="photos"),
     path("<int:pk>/photos/add", views.AddPhotoView.as_view(), name="add-photo"),
     path(
@@ -19,12 +20,4 @@ urlpatterns = [
         views.delete_photo,
         name="delete-photo",
     ),
-    path("<int:pk>/delete/", views.delete_post, name="delete"),
 ]
-"""
-    path(
-        "<int:post_pk>/photos/<int:photo_pk>/edit/",
-        views.EditPhotoView.as_view(),
-        name="edit-photo",
-    ),
-"""
